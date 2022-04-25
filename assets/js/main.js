e68c8df3335a9941f6b51d3b57156a5fc66a2201
Raghav Mrituanjaya <raghav@gogamic.com>
2022-04-25 20:06:21 +0530

    Added code highlighting

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,3 +213,9 @@ function dimmer(action, speed) {
             break;
     }
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre code').forEach((el) => {
+        hljs.highlightElement(el);
+    });
+});
